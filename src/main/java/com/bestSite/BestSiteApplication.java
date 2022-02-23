44bcbc48f23757912eeda817ddb92baff3d56738
Amedomaro <amedomaro13@gmail.com>
2022-02-23 14:52:15 +0300

    Initial commit

---
+++ src/main/java/com/bestSite/BestSiteApplication.java
@@ -0,0 +1,13 @@
+package com.bestSite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BestSiteApplication.class, args);
+	}
+
+}
