82be2593d1407958b31421cacd319b16e5c3d62f
Aleksandr <amedomaro@mail.ru>
2022-02-25 13:26:00 +0300

    delete field status

--- src/main/java/com/bestSite/model/BaseEntity.java
+++ src/main/java/com/bestSite/model/BaseEntity.java
@@ -1,10 +1,7 @@
 package com.bestSite.model;
 
 import lombok.Data;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
 import javax.persistence.*;
-import java.util.Date;
 
 @MappedSuperclass
 @Data
@@ -14,7 +11,4 @@ public class BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
-    @Enumerated(EnumType.STRING)
-    @Column (name = "status")
-    private Status status;
 }
