7f2066dbd8bc8112864ffd6c5d383042ff0755f8
Aleksandr <amedomaro@mail.ru>
2022-02-25 12:05:17 +0300

    add controller Overview

---
+++ src/main/java/com/bestSite/controllers/Overview.java
@@ -0,0 +1,15 @@
+package com.bestSite.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class Overview {
+
+    @GetMapping("/overview")
+    public String showTestPage(Model model){
+        model.addAttribute("title", "overview");
+        return "overview";
+    }
+}
