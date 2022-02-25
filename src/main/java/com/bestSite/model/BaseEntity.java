678a96ff62ad426afd191f3f14b0e6df62520763
Aleksandr <amedomaro@mail.ru>
2022-02-26 00:07:41 +0300

    new GenerationType.AUTO for ID

--- src/main/java/com/bestSite/model/BaseEntity.java
+++ src/main/java/com/bestSite/model/BaseEntity.java
@@ -8,7 +8,7 @@ import javax.persistence.*;
 public class BaseEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
 
 }
