2786a15d36903b10ddde7df21545d23ceea65147
Aleksandr <amedomaro@mail.ru>
2022-03-03 20:53:17 +0300

    Create Comment

---
+++ src/main/java/com/bestSite/model/Comment.java
@@ -0,0 +1,26 @@
+package com.bestSite.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "comments")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Comment extends BaseEntity {
+
+    @Column(name = "message")
+    private String message;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "overview_id")
+    private Overview overview;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id")
+    private User author;
+}
