54207a4675333f88eb38389ae53dc5b7e483da69
Aleksandr <amedomaro@mail.ru>
2022-03-04 16:04:06 +0300

    add @NoArgsConstructor

--- src/main/java/com/bestSite/model/Role.java
+++ src/main/java/com/bestSite/model/Role.java
@@ -1,17 +1,20 @@
 package com.bestSite.model;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
 import java.util.List;
 
 @Entity
 @Table(name = "roles")
+@NoArgsConstructor
 @Data
 public class Role extends BaseEntity{
 
     @Column(name = "name")
     private String name;
 
-    @ManyToMany(mappedBy = "roles", fetch = FetchType.LAZY)
-    private List<User> users;
+//    @ManyToMany(mappedBy = "roles", fetch = FetchType.LAZY)
+//    private List<User> users;
 }
