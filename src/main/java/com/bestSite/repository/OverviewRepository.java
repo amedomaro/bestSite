52b32bd4d57c39bda49d2a202d32ac9d5e61af2c
Aleksandr <amedomaro@mail.ru>
2022-03-04 09:50:35 +0300

    Add getting a list of comments

--- src/main/java/com/bestSite/repository/OverviewRepository.java
+++ src/main/java/com/bestSite/repository/OverviewRepository.java
@@ -2,9 +2,16 @@ package com.bestSite.repository;
 
 import com.bestSite.model.Overview;
 import com.bestSite.model.User;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 import java.util.Optional;
 
 public interface OverviewRepository extends JpaRepository<Overview, Long> {
-    //Optional<User> findByAuthor(User author);
+
+    @EntityGraph(attributePaths = {"comments"})
+    List<Overview> findAll();
+
+    //Optional<Overview> findByAuthor(User author);
 }
