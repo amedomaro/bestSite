1ff161d05239bdf91d4e3c8b020f336b183e6bf2
Aleksandr <amedomaro@mail.ru>
2022-03-06 16:32:33 +0300

    add @AllArgsConstructor

--- src/main/java/com/bestSite/model/Role.java
+++ src/main/java/com/bestSite/model/Role.java
@@ -1,20 +1,28 @@
 package com.bestSite.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
+import org.springframework.security.core.GrantedAuthority;
 import javax.persistence.*;
-import java.util.List;
 
 @Entity
 @Table(name = "roles")
 @NoArgsConstructor
+@AllArgsConstructor
 @Data
-public class Role extends BaseEntity{
+public class Role implements GrantedAuthority {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
 
     @Column(name = "name")
     private String name;
 
-//    @ManyToMany(mappedBy = "roles", fetch = FetchType.LAZY)
-//    private List<User> users;
+    @Override
+    public String getAuthority() {
+        return getName();
+    }
+
 }
