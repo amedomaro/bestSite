3206e85a1fef03af9c19778183d46c4b5937503d
Aleksandr <amedomaro@mail.ru>
2022-03-08 18:59:39 +0300

    add a link to a header-translate.html

--- src/main/java/com/bestSite/repository/OverviewRepository.java
+++ src/main/java/com/bestSite/repository/OverviewRepository.java
@@ -1,12 +1,10 @@
 package com.bestSite.repository;
 
 import com.bestSite.model.Overview;
-import com.bestSite.model.User;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface OverviewRepository extends JpaRepository<Overview, Long> {
 
