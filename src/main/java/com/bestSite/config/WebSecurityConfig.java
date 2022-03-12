864b7b191eb2764eedde7401e5a4d3a5340ea0b6
Aleksandr <amedomaro@mail.ru>
2022-03-12 20:54:08 +0300

    add @EnableGlobalMethodSecurity(prePostEnabled = true)

--- src/main/java/com/bestSite/config/WebSecurityConfig.java
+++ src/main/java/com/bestSite/config/WebSecurityConfig.java
@@ -6,11 +6,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     private PasswordEncoder passwordEncoder;
