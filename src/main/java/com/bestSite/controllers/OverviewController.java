69911f48fbcc9019be66cfdcae72270239bb1b8a
Aleksandr <amedomaro@mail.ru>
2022-03-19 13:36:38 +0300

    now we upload pictures from amazon

--- src/main/java/com/bestSite/controllers/OverviewController.java
+++ src/main/java/com/bestSite/controllers/OverviewController.java
@@ -2,37 +2,35 @@ package com.bestSite.controllers;
 
 import com.bestSite.model.Comment;
 import com.bestSite.model.Overview;
-import com.bestSite.model.User;
 import com.bestSite.repository.CommentRepository;
 import com.bestSite.repository.OverviewRepository;
 import com.bestSite.repository.UserRepository;
+import com.bestSite.service.OverviewService;
+import com.bestSite.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
-import java.util.ArrayList;
 import java.util.Optional;
 
 @Controller
 public class OverviewController {
 
-    private Overview overview;
+    private final OverviewService overviewService;
     private final OverviewRepository overviewRepository;
     private final UserRepository userRepository;
+    private final UserService userService;
     private final CommentRepository commentRepository;
 
     @Autowired
-    public OverviewController(OverviewRepository overviewRepository, UserRepository userRepository,
-                              CommentRepository commentRepository) {
+    public OverviewController(OverviewService overviewService, OverviewRepository overviewRepository, UserRepository userRepository,
+                              UserService userService, CommentRepository commentRepository) {
+        this.overviewService = overviewService;
         this.overviewRepository = overviewRepository;
         this.userRepository = userRepository;
+        this.userService = userService;
         this.commentRepository = commentRepository;
     }
 
@@ -40,15 +38,15 @@ public class OverviewController {
     public String showOverviewPage(Model model) {
         Iterable<Overview> overviews = overviewRepository.findAll();
         model.addAttribute("overviews", overviews);
-        model.addAttribute("title", "overview");
         return "overviews/overview";
     }
 
-//    @GetMapping("/overview/add")
-//    public String addOverview(Model model) {
-//        model.addAttribute("user", userRepository.findByUsername(getCurrentUser().getName()).orElseThrow());
-//        return "overviews/overview-add";
-//    }
+    @GetMapping("/overview/add")
+    public String addOverview(Model model) {
+        model.addAttribute("user",
+                userRepository.findByUsername(userService.getCurrentUser().getName()).orElseThrow());
+        return "overviews/overview-add";
+    }
 //
 //    @PostMapping("/overview/add")
 //    public String addOverview(@RequestParam String title, @RequestParam String image,
@@ -59,9 +57,10 @@ public class OverviewController {
 //        return "redirect:/overview";
 //    }
 
-//    @PreAuthorize("hasAuthority('ADMIN')")
+    //    @PreAuthorize("hasAuthority('ADMIN')")
     @GetMapping("/overview/add/{id}")
-    public String addOverviewFromUser(@PathVariable(value = "id") long id, Model model) {
+    public String addOverviewFromUser(@PathVariable(value = "id") long id, @ModelAttribute("overview") Overview overview,
+                                      Model model) {
         model.addAttribute("user", userRepository.findById(id));
         return "overviews/overview-add";
     }
@@ -69,68 +68,43 @@ public class OverviewController {
     //#user.username.equals(authentication.name)
     //@PreAuthorize("hasAuthority('ADMIN')")
     @PostMapping("/overview/add/{id}")
-    public String addOverviewFromUser(@PathVariable(value = "id") long id,
-                              @RequestParam String title, @RequestParam String image,
-                              @RequestParam String description, @RequestParam String text) {
-        overview = new Overview(title, image, description, text);
-        overview.setAuthor(userRepository.findById(id).orElseThrow());
-        overviewRepository.save(overview);
+    public String addOverviewFromUser(@PathVariable(value = "id") long id, @ModelAttribute("overview") Overview overview,
+                                      @RequestParam Optional<MultipartFile> newImage) {
+        overviewService.saveOverview(id, overview, newImage);
         return "redirect:/overview";
     }
 
     @GetMapping("/overview/{id}")
     public String showDetail(@PathVariable(name = "id") long id, Model model) {
-        if (receiveData(id, model)) return "redirect:/overview";
+        if (overviewService.receiveData(id, model)) return "redirect:/overview";
         Iterable<Comment> comments = commentRepository.findByOverview(overviewRepository.findById(id).orElseThrow());
         model.addAttribute("comments", comments);
         return "overviews/overview-detail";
     }
 
     @PostMapping("/overview/{id}")
-    public String addComment(@PathVariable(value = "id") long id,
-                             @RequestParam(name = "textComment") String text, Model model) {
-        overview = overviewRepository.findById(id).orElseThrow();
-        User user = userRepository.findByUsername(getCurrentUser().getName()).orElseThrow();
-        Comment comment = new Comment(text, overview, user);
-        commentRepository.save(comment);
+    public String addComment(@PathVariable(value = "id") long id, @RequestParam(name = "textComment") String text) {
+        overviewService.saveComment(id, text);
         return "redirect:/overview/{id}";
     }
 
     @GetMapping("/overview/{id}/edit")
     public String edit(@PathVariable(name = "id") long id, Model model) {
-        if (receiveData(id, model)) return "redirect:/overview";
+        if (overviewService.receiveData(id, model)) return "redirect:/overview";
         return "overviews/overview-edit";
     }
 
     @PostMapping("/overview/{id}/edit")
-    public String overviewUpdate(@PathVariable(name = "id") long id, @RequestParam String title, @RequestParam String image,
-                                 @RequestParam String description, @RequestParam String text) {
-        overview = overviewRepository.findById(id).orElseThrow();
-        overview.setTitle(title);
-        overview.setImage(image);
-        overview.setDescription(description);
-        overview.setText(text);
-        overviewRepository.save(overview);
+    public String overviewUpdate(@PathVariable(name = "id") long id, @ModelAttribute("overview") Overview overview,
+                                 @RequestParam Optional<MultipartFile> newImage) {
+        overviewService.editOverview(id, overview, newImage);
         return "redirect:/overview";
     }
 
     @PostMapping("/overview/{id}/delete")
-    public String overviewDelete(@PathVariable(name = "id") long id, Model model) {
-        overview = overviewRepository.findById(id).orElseThrow();
-        overviewRepository.delete(overview);
+    public String overviewDelete(@PathVariable(name = "id") long id) {
+        overviewService.deleteOverview(id);
         return "redirect:/overview";
     }
 
-    private boolean receiveData(@PathVariable(name = "id") long id, Model model) {
-        if (!overviewRepository.existsById(id)) return true;             // check for ID
-        Optional<Overview> overview = overviewRepository.findById(id);
-        ArrayList<Overview> result = new ArrayList<>();
-        overview.ifPresent(result::add);
-        model.addAttribute("overview", result);
-        return false;
-    }
-
-    private Authentication getCurrentUser() {
-        return SecurityContextHolder.getContext().getAuthentication();
-    }
 }
---
+++ src/main/java/com/bestSite/service/OverviewService.java
@@ -0,0 +1,77 @@
+package com.bestSite.service;
+
+import com.bestSite.model.Comment;
+import com.bestSite.model.Overview;
+import com.bestSite.model.User;
+import com.bestSite.repository.CommentRepository;
+import com.bestSite.repository.OverviewRepository;
+import com.bestSite.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+@Service
+public class OverviewService {
+
+    private Overview overview;
+    private final OverviewRepository overviewRepository;
+    private final CommentRepository commentRepository;
+    private final UserRepository userRepository;
+    private final CloudService cloudService;
+    private final UserService userService;
+
+    @Autowired
+    public OverviewService(OverviewRepository overviewRepository, CommentRepository commentRepository,
+                           UserRepository userRepository, CloudService cloudService, UserService userService) {
+        this.overviewRepository = overviewRepository;
+        this.commentRepository = commentRepository;
+        this.userRepository = userRepository;
+        this.cloudService = cloudService;
+        this.userService = userService;
+    }
+
+    public boolean receiveData(@PathVariable(name = "id") long id, Model model) {
+        if (!overviewRepository.existsById(id)) return true;             // check for ID
+        Optional<Overview> overview = overviewRepository.findById(id);
+        ArrayList<Overview> result = new ArrayList<>();
+        overview.ifPresent(result::add);
+        model.addAttribute("overview", result);
+        return false;
+    }
+
+    public void saveOverview(long id, Overview overview, Optional<MultipartFile> newImage){
+        overview.setAuthor(userRepository.findById(id).orElseThrow());
+        String image = cloudService.uploadFile(newImage.orElseThrow());
+        overview.setImage(image);
+        overviewRepository.save(overview);
+    }
+
+    public void deleteOverview(long id){
+        Overview overview = overviewRepository.findById(id).orElseThrow();
+        if(overview.getImage() != null) cloudService.deleteFile(overview.getImage());
+        overviewRepository.delete(overview);
+    }
+
+    public void editOverview(long id, Overview updatedOverview, Optional<MultipartFile> newImage){
+        overview = overviewRepository.findById(id).orElseThrow();
+        String image = cloudService.uploadFile(newImage.orElseThrow());
+        //if(overview.getImage() != null) cloudService.deleteFile(overview.getImage());
+        overview.setImage(image);
+        overview.setTitle(updatedOverview.getTitle());
+        overview.setDescription(updatedOverview.getDescription());
+        overview.setText(updatedOverview.getText());
+        overviewRepository.save(overview);
+    }
+
+    public void saveComment(long id, String text) {
+        Overview overview = overviewRepository.findById(id).orElseThrow();
+        User user = userRepository.findByUsername(userService.getCurrentUser().getName()).orElseThrow();
+        Comment comment = new Comment(text, overview, user);
+        commentRepository.save(comment);
+    }
+}
